ca9dc4c738cba7bc9e3730105ff366db76b36862
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2016-10-16 21:22:41 +0300

    Create Polinoame.h

---
+++ Week-1/Polinoame.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<fstream>
+#include<string.h>
+#include<ctype.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<array>
+#include<deque>
+#include<math.h>
+#include<unordered_set>
+#include<set>
+#include<iomanip>
+#include<bitset>
+
+int cmmdc(int a, int b);
+
+void calculate_gcd(int vec1[], int vec2[], int vec3[]);
+
+int MIN(int x, int y);
