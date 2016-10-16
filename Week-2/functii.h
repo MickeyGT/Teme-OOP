bafb2762ed9ceb1b4eed72e7b0e0ef1ef01c8bd7
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2016-10-16 21:46:30 +0300

    Create functii.h

---
+++ Week-2/functii.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<fstream>
+#include<string.h>
+#include<ctype.h>
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<array>
+#include<deque>
+#include<math.h>
+#include<unordered_set>
+#include<set>
+#include<iomanip>
+#include<bitset>
+
+int calculate_nr_app(char prefix[100], char sir[100]);
