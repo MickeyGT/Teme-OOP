e1256e65dd769d0a0989e39c47b45b79ca457da9
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2016-12-21 18:01:43 +0200

    Update main.cpp

--- Week-10/main.cpp
+++ Week-10/main.cpp
@@ -19,21 +19,59 @@ using namespace std;
 class Bancomat
 {
 private:
-	int bani = 0, plafon = 10000, client[10000],exista[10000];
+	int bani = 0, plafon = 10000, client[10000],savedpin[10000],nrgres[10000];
 public:
 	Bancomat()
 	{
 		for (int i = 0; i <= 9999; i++)
 		{
 			client[i] = 0;
-			exista[i] = 0;
+			savedpin[i] = 0;
+			nrgres[i] = 0;
 		}
+	}
+	void create()
+	{
+		int user,pin;
+		cout << "Ce ID o sa aiba userul?: ";
+		cin >> user;
+		cout << "Ce PIN o sa aiba userul?: ";
+		cin >> pin;
+		savedpin[user] = pin;
+	}
+	int pin(int user)
+	{
+		int pin;
+		cout << "Introduceti pinul: ";
+		cin >> pin;
+		if (savedpin[user] == pin)
+			return 1;
+		else
+			return 0;
 	}
 	void retragere()
 	{
 		int cont;
 		cout << "Dinn ce cont doriti sa retrageti bani? ";
 		cin >> cont;
+		if (nrgres[cont] > 3)
+		{
+			cout << "Cont blocat.\n";
+			return;
+		}
+		try
+		{
+			if (pin(cont) == 0)
+			{
+				nrgres[cont]++;
+				throw "Pinul este gresit.";
+			}
+		}
+		catch (const char* msg)
+		{
+			cout << msg << endl;
+			return;
+		}
 		int sum;
 		cout << "Ce suma doriti sa retrageti?: ";
 		cin >> sum;
@@ -58,6 +96,24 @@ public:
 		int cont;
 		cout << "In ce cont doriti sa adaugati bani? ";
 		cin >> cont;
+		if (nrgres[cont] > 3)
+		{
+			cout << "Cont blocat.\n";
+			return;
+		}
+		try
+		{
+			if (pin(cont) == 0)
+			{
+				nrgres[cont]++;
+				throw "Pinul este gresit.";
+			}
+		}
+		catch (const char* msg)
+		{
+			cout << msg << endl;
+			return;
+		}
 		int sum;
 		cout << "Ce suma doriti sa introduceti?: ";
 		cin >> sum;
@@ -66,10 +122,7 @@ public:
 			if (rand() % 10 == 1)
 				throw "Banii sunt falsi.";
 			else
-			{
 				client[cont] += sum;
-				exista[cont] = 1;
-			}
 		}
 		catch (const char* msg)
 		{
@@ -82,6 +135,24 @@ public:
 		int sender;
 		cout << "Cine este senderul?: ";
 		cin >> sender;
+		if (nrgres[sender] > 3)
+		{
+			cout << "Cont blocat.\n";
+			return;
+		}
+		try
+		{
+			if (pin(sender) == 0)
+			{
+				nrgres[sender]++;
+				throw "Pinul este gresit.";
+			}
+		}
+		catch (const char* msg)
+		{
+			cout << msg << endl;
+			return;
+		}
 		int reciever;
 		cout << "Cine este recieverul?: ";
 		cin >> reciever;
@@ -96,7 +167,7 @@ public:
 				if (sum > plafon)
 					throw "Nu mai e plafon.";
 				else
-					if (exista[reciever] == 0)
+					if (savedpin[reciever] == 0)
 						throw "Nu exista utilizatorul la care sa se faca transferul.";
 					else
 					{
@@ -115,12 +186,24 @@ public:
 int main()
 {
 	Bancomat bcm;
+	bcm.create();
+	bcm.create();
+	bcm.create();
+	bcm.adaugare();
+	bcm.adaugare();
+	bcm.adaugare();
 	bcm.adaugare();
 	bcm.adaugare();
 	bcm.adaugare();
 	bcm.retragere();
 	bcm.retragere();
 	bcm.retragere();
+	bcm.retragere();
+	bcm.retragere();
+	bcm.retragere();
+	bcm.transfer();
+	bcm.transfer();
+	bcm.transfer();
 	bcm.transfer();
 	bcm.transfer();
 	bcm.transfer();
